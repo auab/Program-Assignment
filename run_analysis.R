1c446aea3ac442025841333db7054b3f857b7b14
auab <pedro.aurelio.abreu@gmail.com>
2019-04-09 23:17:53 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,7 @@
 library(reshape2)
-
+#------------------------------------------------------------------------
+# STEP 1
+#------------------------------------------------------------------------
 # Merge the train and test values x 
 train_table_x <- read.table(file = "train/X_train.txt")
 test_table_x <- read.table(file = "test/x_test.txt")
@@ -21,18 +23,28 @@ whole_table_y_activities <- whole_table_y_activities[,-1]
 # Merge x with y values
 whole_table <- cbind(whole_table_x,whole_table_subject,whole_table_y_activities) 
 
-
+#------------------------------------------------------------------------
+# STEP 2
+#------------------------------------------------------------------------
 # Find the columns with mean and std
 features_file <- read.table(file = "features.txt")
 right_columns <- which(grepl("mean|std",features_file[,2],fixed = FALSE))
+
+#------------------------------------------------------------------------
+# STEP 3
+#------------------------------------------------------------------------
 right_names <- c(as.character(features_file[right_columns,2]))
 table_labels <- c("Activity","Subject",right_names)
 
 
-# Table with mean and std values only -> END OF STEP 4
+#------------------------------------------------------------------------
+# STEP 4
+#------------------------------------------------------------------------
 reduced_table <- whole_table[,c(length(whole_table),(length(whole_table)-1),right_columns)]
 colnames(reduced_table) <- table_labels
 
+#------------------------------------------------------------------------
 # STEP 5
+#------------------------------------------------------------------------
 melted_data <- dcast(melt_reduced_table,Activity + Subject ~ variable,mean)
 write.table(melted_data,file = "tidy_data.txt",row.names = FALSE)
